cbdee16850fdf27d710faada21827a57b537bb03
elzemrany <ahmedelzemrany93@gmail.com>
2018-05-24 14:56:22 +0200

    Create bool.py

---
+++ bool.py
@@ -0,0 +1,56 @@
+#
+
+# Function to compare two numbers
+
+#
+
+def compare(a, b):
+
+    if a < b:
+
+        return -1
+
+    elif a > b:
+
+        return 1
+
+    else:
+
+        return 0
+
+#
+
+# a < b condition
+
+retvalue = compare(2, 5)
+
+print (retvalue)
+
+#
+
+# a > b condition
+
+retvalue = compare(5, 2)
+
+print (retvalue)
+
+#
+
+# a == b condition
+
+retvalue = compare(3, 3)
+
+print (retvalue)
+
+Output
+
+>>> 
+
+-1
+
+1
+
+0
+
+#Do not miss to write the flowchart or pseudo code 
+
